5f54743ec90d9c5e5bbf45fe150216d28eb4d3a6
Lucas <lucas.silva@agencian1.com.br>
2024-04-19 02:46:23 -0300

    Outro grande passo

--- react/components/ModalProduct.js
+++ react/components/ModalProduct.js
@@ -5,7 +5,7 @@ import GET_PRODUCT from "../graphql/get_product.gql";
 
 import styles from "../styles/modal-product/styles.css";
 
-const ModalProduct = ({ product_id, state_modal, set_state_modal }) => {
+const ModalProduct = ({ product_id, set_state_pdt, state_pdt }) => {
     const [product, set_product] = useState();
 
     const { loading, error, data } = useQuery(GET_PRODUCT, {
@@ -25,25 +25,27 @@ const ModalProduct = ({ product_id, state_modal, set_state_modal }) => {
 
     if (error) return null;
     if (!product) return null;
-    if(!state_modal) return null;
+    if (state_pdt !== product_id) return null;
 
     return (
         <div className={styles["modal-product-container"]} >
-            <button onClick={()=> set_state_modal(false)} className={styles["modal-product-close-btn"]}>
-                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
-                    <path d="M8.71311 3.20276L2.9043 9.01157" stroke="black" stroke-width="1.83333" stroke-linecap="round" stroke-linejoin="round" />
-                    <path d="M2.9043 3.20276L8.71311 9.01157" stroke="black" stroke-width="1.83333" stroke-linecap="round" stroke-linejoin="round" />
-                </svg>
-            </button>
-            <div className={styles["modal-product-data"]}>
-                <div>
-                    <img src={product.image.imageUrl} alt="Product Image" />
-                </div>
-                <div>
-                    <span>{product.productName}</span>
-                    <span>{product.seller.commertialOffer.ListPrice}</span>
-                    <span>{product.seller.commertialOffer.Price}</span>
-                    <a href={product.link}>ver</a>
+            <div className={styles["modal-product-reference"]}>
+                <button onClick={() => set_state_pdt(null)} className={styles["modal-product-close-btn"]}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+                        <path d="M8.71311 3.20276L2.9043 9.01157" stroke="black" stroke-width="1.83333" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M2.9043 3.20276L8.71311 9.01157" stroke="black" stroke-width="1.83333" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </button>
+                <div className={styles["modal-product-data"]}>
+                    <div className={styles["modal-product-image-content"]}>
+                        <img className={styles["modal-product-image"]} src={product.image.imageUrl} alt="Product Image" />
+                    </div>
+                    <div className={styles["modal-product-info"]}>
+                        <span className={styles["modal-product-name"]}>{product.productName}</span>
+                        <span className={styles["modal-product-list-price"]}>{product.seller.commertialOffer.ListPrice}</span>
+                        <span className={styles["modal-product-price"]}>{product.seller.commertialOffer.Price}</span>
+                        <a className={styles["modal-product-redirect"]} href={product.link}>ver</a>
+                    </div>
                 </div>
             </div>
         </div>
